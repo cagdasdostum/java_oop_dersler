1f73b9423994eaee74fb3d375c93af98af32cc5a
cagdasdostum <cagdascalayir@outlook.com.tr>
2024-11-08 18:30:05 +0300

    package ve importta hata çoıkabilir dikkat

---
+++ Visibility_Modifiers_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+ package com.mycompany.visibility_modifiers_2;
+ import com.mycompany.visibility_modifiers_2.deneme.A;
+ /**
+  *
+  * @author cagda
+  */
+ public class Visibility_Modifiers_2 {
+ 
+     public static void main(String[] args) {
+         A a = new A("Try to create the instance of public constructor outside the package");
+         System.out.println(a.msg);
+         a.Display();
+         // a.info();   // ulaşamıyoruz!!
+     }
+ }
+ 
